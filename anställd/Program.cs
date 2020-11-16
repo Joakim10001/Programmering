2d86cdb31fbde3af7abf25b803433fa8e642ce57
joakim10001 <joakim10001@gmail.com>
2020-11-16 10:02:48 +0100

    w

---
+++ anställd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace anställd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
