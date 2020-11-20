97e1bf994e390b97fc5c07b4959203e2b97fdb09
joakim10001 <joakim10001@gmail.com>
2020-11-20 15:00:52 +0100

    hej

---
+++ anställd/Anställd.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace anställd
+{
+    class Anställd
+    {  
+        protected string n;
+
+        public Anställd(string namn1)
+        {
+            n = namn1;
+        }
+        public double Getlön()
+        {
+
+        }
+        public string
+        
+    }
+}
